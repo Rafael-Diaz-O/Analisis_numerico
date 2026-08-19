ddf53ea1a5021638d11ddd1ee5f0510128717d9e
Rafael Diaz Orostegui <rafadz102@gmail.com>
2026-08-19 09:41:23 -0500

    mejora en la logica de las raices

--- ecuacion_cuadratica_rafael_diaz.m
+++ ecuacion_cuadratica_rafael_diaz.m
@@ -3,22 +3,21 @@ function [x1, x2] = ecuacion_cuadratica_rafael_diaz(a, b, c)
 % 1. Calcular el discriminante
 disc = b^2 - 4*a*c;
 
-% 2. si b == 0, usamos la fórmula estándar simplificada
+% 2. si b == 0, 
 if b == 0
     x1 = sqrt(-c/a);
     x2 = -sqrt(-c/a);
     return;
 end
 
-% 3. Cálculo estable usando la variable auxiliar q
-% Usamos sign(b) para asegurar que sumemos números con el mismo signo
+% 3. Froma normal  
 if b >= 0
-    q = -0.5 * (b + sqrt(disc));
+    q = -(1/2) * (b + sqrt(disc));
 else
-    q = -0.5 * (b - sqrt(disc));
+    q = -(1/2) * (b - sqrt(disc));
 end
 
-% 4. Obtención de las dos raíces
+% 4. raices 
 x1 = q / a;
 x2 = c / q;
 end
